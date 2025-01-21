f3538f8efd7fa7ef9b61f21439d564fa44a47e31
sagakteok <maze77777@naver.com>
2025-01-21 12:57:08 +0900

    First Commit!

---
+++ src/main/java/com/example/Project_Document_ReactSB/ProjectDocumentReactSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_Document_ReactSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDocumentReactSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDocumentReactSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project_Document_ReactSB/ProjectDocumentReactSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_Document_ReactSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDocumentReactSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
